9822f3a27bc13bb4fd6761d14527eb3744dc99b7
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-10-29 00:42:33 +0500

    addind files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Nav } from "./components/NAVBAR/Nav";
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-     
+     <Nav/>
     </React.Fragment>
   );
 }
---
+++ src/components/NAVBAR/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./nav.css"
+import {  } from "../../pics/";
+export const Nav = () => {
+    return (
+        <div>
+        <header className="header">
+           <a href="/" className="logo"> SPA </a>
+           <input className="menu-btn" type="checkbox" id="menu-btn" />
+           <label className="menu-icon" for="menu-btn"><span className="nav-icon"></span></label>
+           <ul className="menu">
+               <li></li>
+               <li></li>
+               <li></li>
+               <li></li>
+               <li></li>
+           </ul>
+       </header>
+   </div>
+    )
+}
