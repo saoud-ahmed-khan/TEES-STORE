04fd9e52aad0520550b3dda5c64fe1433086d02b
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-10-30 23:08:37 +0500

    making header

--- src/components/NAVBAR/Nav.js
+++ src/components/NAVBAR/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./nav.css"
-import logo from "";
+import Header from "../../components/pics/header.jpg";
 export const Nav = () => {
     return (
         <div>
@@ -9,13 +9,14 @@ export const Nav = () => {
            <input className="menu-btn" type="checkbox" id="menu-btn" />
            <label className="menu-icon" for="menu-btn"><span className="nav-icon"></span></label>
            <ul className="menu">
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
+               <li>HOME</li>
+               <li>PRODUUCTS</li>
+               <li>ABOUT US</li>
            </ul>
        </header>
+       <div>
+               <img  className="nav-headerpic" src={Header} alt="header pic"/>
+           </div>
    </div>
     )
 }
