69ab887bcab2e3773d3181802e27c1a774279a4f
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-11-02 01:02:55 +0500

    making product page

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,9 +1,98 @@
 import React from 'react'
 
 export const Products = () => {
+    const tishirts=
+    [
+        {
+            id:1,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:2,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:3,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:4,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:5,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:6,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:7,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:8,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:9,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:10,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:11,
+            title:"",
+            pic:"",
+            price:"",
+        
+        },
+        {
+            id:12,
+            title:"",
+            pic:"",
+            price:"",
+        
+        }
+
+    ]
     return (
-        <div>
-            
-        </div>
+        
+ <div>           
+</div>
+        
     )
 }
