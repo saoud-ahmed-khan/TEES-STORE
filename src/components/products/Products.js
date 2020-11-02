43997cd5e43b913bc1cff4ce706325756023cfb4
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-11-02 22:48:22 +0500

    Update Products.js

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -69,21 +69,21 @@ export const Products = () => {
         {
             id:10,
             title:"",
-            pic:"",
+            pic:"https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201053BLK.JPG_c83d92be-a18f-4521-8534-0f1996607015_800x1000_crop_center.jpg?v=1595847922",
             price:"",
         
         },
         {
             id:11,
             title:"",
-            pic:"",
+            pic:"https://cdn.shopify.com/s/files/1/2290/7887/products/MTP201038-MSTBLK_800x1000_crop_center.jpg?v=1598861094",
             price:"",
         
         },
         {
             id:12,
             title:"",
-            pic:"",
+            pic:"https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201050-WHT_800x1000_crop_center.jpg?v=1598859574",
             price:"",
         
         }
