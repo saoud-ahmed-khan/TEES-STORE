dcc0f73299fda15ab1f5aaa010b6dbf2b691772b
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-11-02 22:55:13 +0500

    Update Products.js

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,98 +1,99 @@
 import React from 'react'
 
 export const Products = () => {
-    const tishirts=
-    [
-        {
-            id:1,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:2,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:3,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:4,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:5,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:6,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:7,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:8,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:9,
-            title:"",
-            pic:"",
-            price:"",
-        
-        },
-        {
-            id:10,
-            title:"",
-            pic:"https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201053BLK.JPG_c83d92be-a18f-4521-8534-0f1996607015_800x1000_crop_center.jpg?v=1595847922",
-            price:"",
-        
-        },
-        {
-            id:11,
-            title:"",
-            pic:"https://cdn.shopify.com/s/files/1/2290/7887/products/MTP201038-MSTBLK_800x1000_crop_center.jpg?v=1598861094",
-            price:"",
-        
-        },
-        {
-            id:12,
-            title:"",
-            pic:"https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201050-WHT_800x1000_crop_center.jpg?v=1598859574",
-            price:"",
-        
-        }
+    const tishirts =
+        [
+            {
+                id: 1,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTP101124HGRBLK_1_800x1000_crop_center.jpg?v=1599580652",
+                price: "",
 
-    ]
+            },
+            {
+                id: 2,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201055MRNHTG_1_800x1000_crop_center.jpg?v=1599670266",
+                price: "",
+
+            },
+            {
+                id: 3,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTP201042CHR_4_800x1000_crop_center.jpg?v=1599580133",
+                price: "",
+
+            },
+            {
+                id: 4,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201049-OLV_1_dde6f304-638a-4581-9d4f-98b5f1c2f692_800x1000_crop_center.jpg?v=1592900236",
+                price: "",
+
+            },
+            {
+                id: 5,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTP201056-NVY_800x1000_crop_center.jpg?v=1592900967",
+                price: "",
+
+            },
+            {
+                id: 6,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTP201064RED_2_800x1000_crop_center.jpg?v=1601360097",
+                price: "",
+
+            },
+            {
+                id: 7,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201054-MST_800x1000_crop_center.jpg?v=1592392135",
+                price: "",
+
+            },
+            {
+                id: 8,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201046-HTRGRY_800x1000_crop_center.jpg?v=1594475863",
+                price: "",
+
+            },
+            {
+                id: 9,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201058-RED_1_800x1000_crop_center.jpg?v=1598519731",
+                price: "",
+
+            },
+            {
+                id: 10,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201053BLK.JPG_c83d92be-a18f-4521-8534-0f1996607015_800x1000_crop_center.jpg?v=1595847922",
+                price: "",
+
+            },
+            {
+                id: 11,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTP201038-MSTBLK_800x1000_crop_center.jpg?v=1598861094",
+                price: "",
+
+            },
+            {
+                id: 12,
+                title: "",
+                pic: "https://cdn.shopify.com/s/files/1/2290/7887/products/MTF201050-WHT_800x1000_crop_center.jpg?v=1598859574",
+                price: "",
+
+            }
+
+        ]
     return (
-        
- <div>           
-</div>
-        
+
+        <div className="Product-main">
+            
+        </div>
+
     )
 }
