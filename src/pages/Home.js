635258709bbf7934da596b7a9c90feca0d9a4e0d
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-11-03 13:01:06 +0500

    adding components

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CardSection } from "./../components/CARDS/CardSection/CardSection";
+import { Section } from "./../components/curvediv/Section";
+export const Home = () => {
+    return (
+        <div>
+            <CardSection/>
+            <Section/>
+        </div>
+    )
+}
